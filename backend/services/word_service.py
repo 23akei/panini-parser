920d825c00434575b64b27acca4cde4d7c4b75b2
Daichi Suzuki <mathmathds@gmail.com>
2025-07-03 12:29:05 +0900

    use simple words and rules for begginer mode

--- backend/services/word_service.py
+++ backend/services/word_service.py
@@ -13,20 +13,30 @@ class WordService:
     def __init__(self, kosha:Kosha ):
         self._v = Vyakarana()
         self.kosha:list[DhatuEntry] = list(kosha.dhatus())
+        self.kosha_begginer = list(filter(lambda d:  (d.dhatu.gana in [Gana.Bhvadi, Gana.Divadi, Gana.Tudadi, Gana.Curadi]) and d.dhatu.sanadi == [] and d.dhatu.prefixes==[] , self.kosha))
     
-    def get_random_dhatu(self) -> Optional[Dhatu]:
+    def get_random_dhatu(self, level:str) -> Optional[Dhatu]:
         """ Get a random root word (dhatu) for gameplay """
-        return random.choice(self.kosha).dhatu
-    def get_random_prakriya(self, dhatu) -> Optional[Prakriya]:
+        if level not in ["beginner", "expert"]:
+            raise ValueError(f"Invalid level: {level}")
+        if level == "beginner":
+            # For beginner level, return a simple dhatu
+            return random.choice(self.kosha_begginer).dhatu
+        elif level == "expert":
+            return random.choice(self.kosha).dhatu
+        else:
+            raise ValueError(f"Invalid level: {level}")
+    def get_random_prakriya(self, dhatu, level: str) -> Optional[Prakriya]:
         # TODO: Implement logic to fetch a random dhatu or pratipadika
-        return self.get_random_tinanta_prakriya(dhatu)
+        return self.get_random_tinanta_prakriya(dhatu, level)
 
-    def get_random_tinanta_prakriya(self, dhatu) -> Optional[Prakriya]:
+    def get_random_tinanta_prakriya(self, dhatu, level: str) -> Optional[Prakriya]:
         """ Get a random prakriya (word parsing) for gameplay """
-        prayoga = random.choice(Prayoga.choices())
-        lakara = random.choice(Lakara.choices())
-        purusha = random.choice(Purusha.choices())
-        vacana = random.choice(Vacana.choices())
+        prayoga = self._get_random_prayoaga(level)
+        lakara = self._get_random_lakara(level)
+        purusha = self._get_random_purusha(level)
+        vacana =  self._get_random_vacana(level)
+        print(f"Generating prakriya for dhatu: {dhatu}, prayoga: {prayoga}, lakara: {lakara}, purusha: {purusha}, vacana: {vacana}")
         prakriyas = self._v.derive(Pada.Tinanta(
             dhatu=dhatu,
             prayoga=prayoga,
@@ -39,6 +49,33 @@ class WordService:
         if len(prakriyas) > 1:
             return random.choice(prakriyas)
         return prakriyas[0]
+    def _get_random_prayoaga(self, level: str) -> Prayoga:
+        """ Get a random prayoga based on difficulty level """
+        if level == "beginner":
+            return Prayoga.Kartari
+        elif level == "expert":
+            return random.choice(Prayoga.choices())
+        else:
+            raise ValueError(f"Invalid level: {level}")
+    def _get_random_lakara(self, level: str) -> Lakara:
+        """ Get a random lakara based on difficulty level """
+        if level == "beginner":
+            return Lakara.Lat
+        elif level == "expert":
+            return random.choice(Lakara.choices())
+        else:
+            raise ValueError(f"Invalid level: {level}")
+    def _get_random_purusha(self, level: str) -> Purusha:
+        return random.choice(Purusha.choices())
+    def _get_random_vacana(self, level: str) -> Vacana:
+        """ Get a random vacana based on difficulty level """
+        if level == "beginner":
+            return random.choice([Vacana.Eka, Vacana.Bahu])
+        elif level == "expert":
+            return random.choice(Vacana.choices())
+        else:
+            raise ValueError(f"Invalid level: {level}")
+    
     def get_random_subanta_prakriya(self, pratipadika) -> Optional[Prakriya]:
         linga = random.choice(Linga.choices())
         vibhakti = random.choice(Vibhakti.choices())
